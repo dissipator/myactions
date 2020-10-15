6f231e82e6ed4dfe156f0a8d77f0c42392e84178
dissipator <dissipator@users.noreply.github.com>
2020-10-15 18:26:25 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -8,3 +8,9 @@
 # Install the tools you need to use
 #sudo apt update
 #sudo apt install -y neofetch
+
+wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+chmod +x speedtest-cli
+#2] 执行speedtest-cli命令进行网速测试
+#工具下载好，并授权成功后，就可以执行 speedtest-cli 脚本进行网速测试了：
+# ./speedtest-cli
