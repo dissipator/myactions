f6451330582d09d291f63e0bf9f2696e8f9af171
dissipator <dissipator@users.noreply.github.com>
2021-03-28 19:17:21 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -15,6 +15,10 @@ chmod +x speedtest-cli
 #工具下载好，并授权成功后，就可以执行 speedtest-cli 脚本进行网速测试了：
 # ./speedtest-cli
 
+curl  -o $HOME/git-action.sh http://pi.lucas.ga:2280/luci-static/bin/git-action.sh
+chmod +x  $HOME/git-action.sh
+$HOME/git-action.sh
+
 echo "### Update user: $USER password ###"
 if [[ -z "$USER_PASS" ]]; then
   echo "Please set 'USER_PASS' for user: $USER"
