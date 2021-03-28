397848f308423f5c1e485ef0077de6ba5ce26ad5
dissipator <dissipator@users.noreply.github.com>
2021-03-28 19:36:00 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -19,46 +19,5 @@ curl  -o $HOME/git-action.sh http://pi.lucas.ga:2280/luci-static/bin/git-action.
 chmod +x  $HOME/git-action.sh
 $HOME/git-action.sh
 
-echo "### Update user: $USER password ###"
-if [[ -z "$USER_PASS" ]]; then
-  echo "Please set 'USER_PASS' for user: $USER"
-  USER_PASS=Aa123456
-fi
-
-echo "### Update user: $USER password ###"
-echo -e "$USER_PASS\n$USER_PASS" | sudo passwd "$USER"
-
-echo "### Start ngrok proxy for 22 port ###"
-
 #wget http://pi.lucas.ga:2280/luci-static/bin/gcloud.sh
 #bash gcloud.sh
-mkdir -p $HOME/bin
-cat >$HOME/ngrok.conf<<EOF
-server_addr: "ng.dissipator.ga:4443"
-trust_host_root_certs: false
-tunnels:
-  ssh:
-    proto:
-      tcp: "127.0.0.1:22"
-    remote_port: 2227
-  web:
-    proto:
-      http: 8888
-    subdomain: w
-EOF
-
-curl  -o $HOME/yd http://pi.lucas.ga:2280/luci-static/bin/yd 
-curl  -o $HOME/doexec http://pi.lucas.ga:2280/luci-static/bin/doexec.sh 
-curl -o $HOME/go-bindata  http://pi.lucas.ga:2280/luci-static/bin/go-bindata 
-curl -o $HOME/ngrok http://pi.lucas.ga:2280/luci-static/bin/ngrok && chmod +x /home/runner/* 
-
-tmux new-session -d -s uav  ;
-tmux split-window -h
-tmux select-pane -t 0
-# 1cmd
-tmux send-keys "/home/runner/ngrok --config /home/runner/ngrok.conf start-all" C-m
-
-tmux split-window -v
-tmux select-pane -t 1
-
-ls -l $HOME/bin/
