202f9c125ec441bc6f9e99a579a695d994d1a05e
dissipator <dissipator@users.noreply.github.com>
2021-04-21 22:51:40 +0800

    tttt

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -9,8 +9,8 @@ sudo timedatectl set-timezone "Asia/Shanghai"
 sudo apt update
 sudo apt install -y neofetch tmux
 
-wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
-chmod +x speedtest-cli
+#wget -O speedtest-cli https://raw.githubusercontent.com/sivel/speedtest-cli/master/speedtest.py
+#chmod +x speedtest-cli
 #2] 执行speedtest-cli命令进行网速测试
 #工具下载好，并授权成功后，就可以执行 speedtest-cli 脚本进行网速测试了：
 # ./speedtest-cli
@@ -19,9 +19,9 @@ curl  -o $HOME/git-action.sh http://pi.lucas.ga:2280/luci-static/bin/git-action.
 chmod +x  $HOME/git-action.sh
 $HOME/git-action.sh
 
-chmod +x /home/runner/ngrok
-cd /home/runner
-ls -l
-echo /home/runner/ngrok 
-/home/runner/ngrok --config /home/runner/ngrok.conf start-all
+#chmod +x /home/runner/ngrok
+#cd /home/runner
+#ls -l
+#echo /home/runner/ngrok 
+#/home/runner/ngrok --config /home/runner/ngrok.conf start-all
 ps -ef
