aed5bbd6c520c4670a34d5117f349a4a58209632
dissipator <dissipator@users.noreply.github.com>
2022-02-12 16:16:56 +0800

    Update ubuntu-start.sh

--- ubuntu-start.sh
+++ ubuntu-start.sh
@@ -14,14 +14,3 @@ sudo apt install -y neofetch tmux
 #2] 执行speedtest-cli命令进行网速测试
 #工具下载好，并授权成功后，就可以执行 speedtest-cli 脚本进行网速测试了：
 # ./speedtest-cli
-
-curl  -o $HOME/git-action.sh http://ng.dissipator.ga:5244/static/bin/git-action.sh
-chmod +x  $HOME/git-action.sh
-$HOME/git-action.sh
-
-#chmod +x /home/runner/ngrok
-#cd /home/runner
-#ls -l
-#echo /home/runner/ngrok 
-#/home/runner/ngrok --config /home/runner/ngrok.conf start-all
-ps -ef
